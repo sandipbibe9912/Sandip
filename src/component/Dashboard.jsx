4e56cf5c63d62c2c067321a2d14f19b6666a4a2b
Sandipbibe99 <156405495+Sandipbibe99@users.noreply.github.com>
2024-05-15 10:08:24 +0530

    first commit

---
+++ src/component/Dashboard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Appbar from './Appbar'
+
+const Dashboard = () => {
+  return (
+  
+    <div style={{}}>
+    <h1>Dashboard</h1>
+    </div>
+ 
+  )
+}
+
+export default Dashboard
